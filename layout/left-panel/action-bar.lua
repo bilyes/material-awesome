b15757dae8e2e1a7e90e8abdfd963c54af216443
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-12-05 21:00:33 -0500

    Add a ram widget on the left panel.

--- layout/left-panel/action-bar.lua
+++ layout/left-panel/action-bar.lua
@@ -91,6 +91,7 @@ return function(screen, panel, action_bar_width)
       layout = wibox.layout.fixed.vertical,
       wibox.container.margin(systray, dpi(10), dpi(10)),
       --require('widget.package-updater'),
+      wibox.container.margin(require('widget.ram.ram-widget'), dpi(6), dpi(6)),
       require('widget.wifi'),
       require('widget.battery'),
       -- Clock
---
+++ widget/ram/ram-widget.lua
@@ -0,0 +1,62 @@
+local awful = require("awful")
+local watch = require("awful.widget.watch")
+local wibox = require("wibox")
+local dpi = require('beautiful').xresources.apply_dpi
+
+local ramgraph_widget = {}
+
+local args = args or {}
+local timeout = args.timeout or 1
+
+ramgraph_widget = wibox.widget {
+    border_width = 0,
+    max_value = 1,
+    value = 0.5,
+    paddings = 1,
+    forced_width = dpi(24),
+    color = "#339933",
+    background_color = "#264d00",
+    widget = wibox.widget.progressbar
+}
+
+--local ram_popup =
+  --awful.tooltip(
+  --{
+    --objects = {ramgraph_widget},
+    --mode = 'outside',
+    --align = 'left',
+    --preferred_positions = {'right', 'left', 'top', 'bottom'}
+  --}
+--)
+
+local function getPercentage(value, total)
+    return math.floor(value / total * 100 + 0.5) .. '%'
+end
+
+local usage = wibox.widget {
+    text = "",
+    align = 'center',
+    valign = 'center',
+    opacity = 0.5,
+    font = 'Roboto Mono 8',
+    widget = wibox.widget.textbox
+}
+
+watch('bash -c "LANGUAGE=en_US.UTF-8 free | grep -z Mem.*Swap.*"', timeout,
+    function(widget, stdout, stderr, exitreason, exitcode)
+        local total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap =
+            stdout:match('(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*Swap:%s*(%d+)%s*(%d+)%s*(%d+)')
+
+        --percentage_value = getPercentage(used, total)
+        widget.value = used / total
+        usage.text = getPercentage(used, total)
+        --ram_popup.text = usage.text
+    end,
+    ramgraph_widget
+)
+
+return wibox.widget {
+    wibox.container.rotate(ramgraph_widget, 'east'),
+    usage,
+    layout = wibox.layout.stack
+}
