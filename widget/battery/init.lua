2d1f1d4c662f01a4e68a263ffd9f8ed982f7c481
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-12-24 14:44:31 -0500

    Widget: Refactor the battery widget.

--- widget/battery/init.lua
+++ widget/battery/init.lua
@@ -1,27 +1,25 @@
 -------------------------------------------------
 -- Battery Widget for Awesome Window Manager
 -- Shows the battery status using the ACPI tool
--- More details could be found here:
--- https://github.com/streetturtle/awesome-wm-widgets/tree/master/battery-widget
 
--- @author Pavel Makhov
--- @copyright 2017 Pavel Makhov
+-- @author Ilyess Bachiri
+-- @copyright 2021-present Ilyess Bachiri
 -------------------------------------------------
 
 local awful = require('awful')
+local clickable_container = require('widget.material.clickable-container')
+local dpi = require('beautiful').xresources.apply_dpi
+local filesystem = require('gears.filesystem')
+local gears = require('gears')
 local naughty = require('naughty')
 local watch = require('awful.widget.watch')
 local wibox = require('wibox')
-local clickable_container = require('widget.material.clickable-container')
-local gears = require('gears')
-local dpi = require('beautiful').xresources.apply_dpi
 
 -- acpi sample outputs
 -- Battery 0: Discharging, 75%, 01:51:38 remaining
 -- Battery 0: Charging, 53%, 00:57:43 until charged
 
-local HOME = os.getenv('HOME')
-local PATH_TO_ICONS = HOME .. '/.config/awesome/widget/battery/icons/'
+local PATH_TO_ICONS = filesystem.get_configuration_dir() .. '/widget/battery/icons/'
 
 local widget =
   wibox.widget {
@@ -80,66 +78,80 @@ end
 
 local last_battery_check = os.time()
 
+function get_icon_name(charge)
+  local iconName = 'battery'
+
+  if status == 'Charging' or status == 'Full' then
+    iconName = iconName .. '-charging'
+  end
+
+  local roundedCharge = math.floor(charge / 10) * 10
+  if (roundedCharge == 0) then
+    iconName = iconName .. '-outline'
+  elseif (roundedCharge ~= 100) then
+    iconName = iconName .. '-' .. roundedCharge
+  end
+
+  return iconName
+end
+
+function notify_if_low(charge)
+  if (charge >= 0 and charge < 15) then
+    if status ~= 'Charging' and os.difftime(os.time(), last_battery_check) > 300 then
+      -- if 5 minutes have elapsed since the last warning
+      last_battery_check = _G.time()
+
+      show_battery_warning()
+    end
+  end
+end
+
+local update_widget = function(_, stdout)
+
+  local battery_info = {}
+  local capacities = {}
+  for s in stdout:gmatch('[^\r\n]+') do
+    local status, charge_str, time = string.match(s, '.+: (%a+), (%d?%d?%d)%%,?.*')
+    if status ~= nil then
+      table.insert(battery_info, {status = status, charge = tonumber(charge_str)})
+    else
+      local cap_str = string.match(s, '.+:.+last full capacity (%d+)')
+      table.insert(capacities, tonumber(cap_str))
+    end
+  end
+
+  local capacity = 0
+  for _, cap in ipairs(capacities) do
+    capacity = capacity + cap
+  end
+
+  local charge = 0
+  local status
+  for i, batt in ipairs(battery_info) do
+    if batt.charge >= charge then
+      status = batt.status -- use most charged battery status
+    -- this is arbitrary, and maybe another metric should be used
+    end
+
+    charge = charge + batt.charge
+
+    if capacities[i]  ~= nil then
+        charge = charge * capacities[i]
+    end
+  end
+  charge = charge / capacity
+
+  notify_if_low(charge)
+  widget.icon:set_image(PATH_TO_ICONS .. get_icon_name(charge) .. '.svg')
+  -- Update popup text
+  battery_popup.text = string.gsub(stdout, '\n$', '')
+  collectgarbage('collect')
+end
+
 watch(
   'acpi -i',
   1,
-  function(_, stdout)
-    local batteryIconName = 'battery'
-
-    local battery_info = {}
-    local capacities = {}
-    for s in stdout:gmatch('[^\r\n]+') do
-      local status, charge_str, time = string.match(s, '.+: (%a+), (%d?%d?%d)%%,?.*')
-      if status ~= nil then
-        table.insert(battery_info, {status = status, charge = tonumber(charge_str)})
-      else
-        local cap_str = string.match(s, '.+:.+last full capacity (%d+)')
-        table.insert(capacities, tonumber(cap_str))
-      end
-    end
-
-    local capacity = 0
-    for _, cap in ipairs(capacities) do
-      capacity = capacity + cap
-    end
-
-    local charge = 0
-    local status
-    for i, batt in ipairs(battery_info) do
-      if batt.charge >= charge then
-        status = batt.status -- use most charged battery status
-      -- this is arbitrary, and maybe another metric should be used
-      end
-
-      charge = charge + batt.charge * capacities[i]
-    end
-    charge = charge / capacity
-
-    if (charge >= 0 and charge < 15) then
-      if status ~= 'Charging' and os.difftime(os.time(), last_battery_check) > 300 then
-        -- if 5 minutes have elapsed since the last warning
-        last_battery_check = _G.time()
-
-        show_battery_warning()
-      end
-    end
-
-    if status == 'Charging' or status == 'Full' then
-      batteryIconName = batteryIconName .. '-charging'
-    end
-
-    local roundedCharge = math.floor(charge / 10) * 10
-    if (roundedCharge == 0) then
-      batteryIconName = batteryIconName .. '-outline'
-    elseif (roundedCharge ~= 100) then
-      batteryIconName = batteryIconName .. '-' .. roundedCharge
-    end
-
-    widget.icon:set_image(PATH_TO_ICONS .. batteryIconName .. '.svg')
-    -- Update popup text
-    battery_popup.text = string.gsub(stdout, '\n$', '')
-    collectgarbage('collect')
-  end,
+  update_widget,
   widget
 )
 
